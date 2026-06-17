b81d279686c48e7a9616edd29bdac1a22a82293e
Priyanka Sundalam <ksundalam@gmail.com>
2026-06-17 20:42:20 +0530

    Solved the very 1st CP problem

--- A_Halloumi_Boxes.java
+++ A_Halloumi_Boxes.java
@@ -4,36 +4,35 @@ public class A_Halloumi_Boxes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        for(int i=0; i<t; i++){
+
+        while(t-- != 0){
             int n = sc.nextInt(), k = sc.nextInt();
-            int[] arr =new int[n];
-            for(int j=0; j<n; j++){
-                arr[j] = sc.nextInt();
+            int arr[] = new int[n];
+            for(int i=0; i<n; i++){
+                arr[i] = sc.nextInt();
             }
-            String ans = solve(n, k, arr);
-            System.out.println(ans);
+
+            //now we have the k which is max subarr len that we can reverse in an array and the array itself
+            boolean res = checkIfSortable(arr, k, n);
+
+            System.out.println(res? "YES" : "NO");
         }
-        sc.close();
-    }   
-    public static String solve(int n, int k, int[] arr){
-        if(k == 1){
-            //which means the subarr len for reversal is 1 
-            //so, we've to check if the arr is already sorted, then only return yes
-            if(isSorted(arr, n))
-                return "YES";
-            else    
-                return "NO";
-        }
-        //if k > 1, we can always "always" make the array sorted by just swapping adj eles (bubble sort) 
-        //cuz any no of reverses of subarrays are allowed and subarray can be of any len bet 1 and k
-        return "YES";
     }
-    public static boolean isSorted(int[] arr, int n){
-        for(int i=1; i<n; i++){
-            if(arr[i]<arr[i-1]){
-                return false;
+    public static boolean checkIfSortable(int[] arr, int k, int n){
+        if(k > 1){
+            //we can just simply swap the adj elemennts becuz the subarr leng to reverse is atmost k
+            //which is simply like using bubble sort algo
+            return true;
+        }else if(k == 1){
+            //if k is 1 then we can't swap or reverse the elements of arr
+            //hence we need to check if the arr is already srted or not to get the result
+            for(int i=0; i<n-1; i++){
+                if(arr[i] > arr[i+1]){
+                    return false;
+                }
             }
+            return true;
         }
-        return true;
+        return false;
     }
 }
